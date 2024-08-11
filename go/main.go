fbcb95b1f1e576504bb52e501270d6c110106145
masami <msihima0304@gmail.com>
2024-08-11 18:43:26 +0900

    Update maximum database connections to 50

--- go/main.go
+++ go/main.go
@@ -101,8 +101,8 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.SetMaxOpenConns(100)
-	db.SetMaxIdleConns(100)
+	db.SetMaxOpenConns(50)
+	db.SetMaxIdleConns(50)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
