ee73d6c04d8cd4c1c57550f7e62f09f34b14ccb3
masami <msihima0304@gmail.com>
2024-08-11 14:26:11 +0900

    Update user registration to use specific IP and TTL value

--- go/user_handler.go
+++ go/user_handler.go
@@ -272,10 +272,7 @@ func registerHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to insert user theme: "+err.Error())
 	}
 
-	cmd := exec.Command("ssh", "isucon-s1",
-		"pdnsutil", "add-record", "t.isucon.pw", req.Name, "A", "0", powerDNSSubdomainAddress)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
+	if out, err := exec.Command("ssh", "192.168.0.11", "pdnsutil", "add-record", "t.isucon.pw", req.Name, "A", "60", powerDNSSubdomainAddress).CombinedOutput(); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, string(out)+": "+err.Error())
 	}
 
